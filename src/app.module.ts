3e39d36d3336ed5bf27ba6b6c14c0e1c6ffcd148
Sergio Penaloza <sergioandrespegel@gmail.com>
2024-12-17 18:03:57 -0500

    Actualiza archivos de configuración, elimina archivo de prueba y ajusta la lógica en varios servicios y controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GmailModule } from './gmail/gmail.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), GmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
